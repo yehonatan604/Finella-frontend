e5ceb5a32fa91a5c80e584047aeef2f64704e123
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-04-21 22:42:38 +0300

    cleanup

--- src/Core/store/socketSlice.ts
+++ src/Core/store/socketSlice.ts
@@ -29,23 +29,12 @@ const socketSlice = createSlice({
                 state.connected = true;
 
                 const connectionEvents = ["connect", "disconnect"];
-                //const alertEvents = ["note-automation-triggered", "todo-failed"];
 
                 connectionEvents.forEach((event) => {
                     socket.on(event, () => {
                         console.log(`${event === "connect" ? "✅" : "❌"} ${event}ed`);
                     });
                 });
-
-                // alertEvents.forEach((event) => {
-                //     socket.on(event, (args) => {
-                //         alert(args.title, args.content, "info");
-
-                //         if (args.id) {
-                //             socket.emit("note-read", { id: args.id });
-                //         }
-                //     });
-                // });
 
                 socket.on("connect_error", (err) => {
                     console.error("❌ connection error:", err.message);
